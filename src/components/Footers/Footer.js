079478921c5aa0b36e0a40a6d419ea3d3520bf8e
Satheeshwaran J <satheesh4590@gmail.com>
2023-03-20 12:30:03 +0530

    Welcome page commit.

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -12,30 +12,10 @@ export default function Footer() {
                 <div className="w-full lg:w-4/12 px-4">
                   <ul className="list-unstyled">
                     <li>
-                      <a
-                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                        href="https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md?ref=nr-footer"
-                      >
-                        MIT License
-                      </a>
+                       SnowWhiteCryos
                     </li>
                   </ul>
                 </div>
-              </div>
-            </div>
-          </div>
-          <hr className="my-6 border-blueGray-300" />
-          <div className="flex flex-wrap items-center md:justify-between justify-center">
-            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div className="text-sm text-blueGray-500 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} Notus React by{" "}
-                <a
-                  href="https://www.creative-tim.com?ref=nr-footer"
-                  className="text-blueGray-500 hover:text-blueGray-800"
-                >
-                  Creative Tim
-                </a>
-                .
               </div>
             </div>
           </div>
